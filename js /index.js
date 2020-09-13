b53fe0f1714b111d75d559f74579b1883d7385f7
vkumnoonsate <62566689+vkumnoonsate@users.noreply.github.com>
2020-09-12 23:23:46 -0700

    design updates

--- js /index.js
+++ js /index.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
-	$('.openresume').click(function(){
-   		$('.resume').show();
-    }); 
-}); 
+// Click function for show the Modal
 
+$(".show").on("click", function(){
+	$(".mask").addClass("active");
+  });
+  
+  // Function for close the Modal
+  
+  function closeModal(){
+	$(".mask").removeClass("active");
+  }
+  
+  // Call the closeModal function on the clicks/keyboard
+  
+  $(".close, .mask").on("click", function(){
+	closeModal();
+  });
+  
+  $(document).keyup(function(e) {
+	if (e.keyCode == 27) {
+	  closeModal();
+	}
+  });
